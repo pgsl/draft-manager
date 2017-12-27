2c0335201f060ca1e46c992a0db58ecea32828b1
Darian Shimy <dshimy@gmail.com>
2017-12-26 21:47:40 -0800

    Allow web console debugging in docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Used when debugging in the docker container.
+  config.web_console.whitelisted_ips = '172.18.0.0/16'
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
